6c7ed6c082f36de24f99480d37b20324eb583e7a
Criss-Wang <djj87018507@gmail.com>
2022-01-30 02:01:31 +0800

    Fix small bugs from unordered_map behaviour - contains function not included in current STL

--- Team39/Code39/src/spa/src/PKB/entity_table.cpp
+++ Team39/Code39/src/spa/src/PKB/entity_table.cpp
@@ -1,20 +1,22 @@
 #include "entity_table.h"
 
-bool EntityTable::CheckValidEntityIdx(const int entity_idx)
-{
-	if (entity_table_.find(entity_idx) != entity_table_.end())
-	{
-		return true;
-	} 
-	{
-		return false;
-	}
-}
+//template <typename T1, typename T2>
+//bool EntityTable<T1,T2>::CheckValidEntityKey(const T1 entity_idx)
+//{
+//	if (entity_table_.contains(entity_idx))
+//	{
+//		return true;
+//	} 
+//	{
+//		return false;
+//	}
+//}
 
-Ull EntityTable::GetTableSize()
-{
-	return entity_table_.size();
-}
+//template <typename T1, typename T2>
+//Ull EntityTable::GetTableSize()
+//{
+//	return entity_table_.size();
+//}
 
 //StmtTable::StmtTable() = default;
 
@@ -23,66 +25,65 @@ Ull EntityTable::GetTableSize()
 //	delete[] &entity_table_;
 //}
 
+//int StmtTable::AddEntity(const int stmt_idx, const int stmt_prop)
+//{
+//	try
+//	{
+//		if (CheckValidProp(stmt_prop))
+//		{
+//			entity_table_[stmt_idx] = stmt_prop;
+//			return 1;
+//		} 
+//		{
+//			throw invalid_argument(to_string(stmt_prop));
+//		}
+//	} catch (invalid_argument& e)
+//	{
+//		// TODO(Zhenlin): add error to the logger if possible
+//		return 0;
+//	}
+//}
+
+//int StmtTable::GetEntityByKey(const int stmt_idx)
+//{
+//	try
+//	{
+//		if (CheckValidEntityKey(stmt_idx))
+//		{
+//			return entity_table_[stmt_idx];
+//		}
+//		{
+//			throw invalid_argument(to_string(stmt_idx));
+//		}
+//	}
+//	catch (invalid_argument& e)
+//	{
+//		// TODO(Zhenlin): add error to the logger if possible
+//		return 0;
+//	}
+//}
+
 bool StmtTable::CheckValidProp(const int stmt_prop)
 {
-	if (stmt_prop >= assign_idx_ && stmt_prop <= while_idx_)
-	{
-		return true;
-	} 
-	{
-		return false;
-	}
+	return (stmt_prop >= assign_idx_ && stmt_prop <= while_idx_);
 }
 
-
-int StmtTable::AddEntity(const int stmt_idx, const int stmt_prop)
+bool StmtTable::CheckValidEntityKey(const int stmt_idx)
 {
-	try
-	{
-		if (CheckValidProp(stmt_prop))
-		{
-			entity_table_[stmt_idx] = stmt_prop;
-			return 1;
-		} 
-		{
-			throw invalid_argument(to_string(stmt_prop));
-		}
-	} catch (invalid_argument& e)
-	{
-		// TODO(Zhenlin): add error to the logger if possible
-		return 0;
-	}
+	return stmt_idx >= initial_index_;
 }
 
-int StmtTable::GetEntityByIdx(const int stmt_idx)
-{
-	try
-	{
-		if (CheckValidEntityIdx(stmt_idx))
-		{
-			return entity_table_[stmt_idx];
-		}
-		{
-			throw invalid_argument(to_string(stmt_idx));
-		}
-	}
-	catch (invalid_argument& e)
-	{
-		// TODO(Zhenlin): add error to the logger if possible
-		return 0;
-	}
-}
 
-vector<int> StmtTable::GetEntityLstByProp(const int stmt_prop)
+vector<int> StmtTable::GetStmtLstByProp(const int stmt_prop)
 {
 	vector<int> results;
 	try
 	{
-		if (CheckValidProp(stmt_prop))
+		if (this->CheckValidProp(stmt_prop))
 		{
 			for (const auto & [key, value] : entity_table_)
 			{
-				if ((key) == stmt_prop) results.push_back(value);
+				if ((value) == stmt_prop) results.push_back(key);
 			}
 			return results;
 		}
@@ -98,13 +99,40 @@ vector<int> StmtTable::GetEntityLstByProp(const int stmt_prop)
 	}
 }
 
-bool NonStmtTable::CheckValidProp(const int var_prop)
+bool NonStmtTable::CheckValidProp(const int var_id)
 {
-	if (var_prop >= var_idx_ && var_prop <= proc_idx_)
+	return var_id >= initial_id_ && var_id <= GetTableSize();
+}
+
+/**
+ * Set to true currently. Will make changes if any appropriate criteria surfaces.
+ */
+bool NonStmtTable::CheckValidEntityKey(const string var_name)
+{
+	return true;
+}
+
+string NonStmtTable::GetVarById(const int var_id)
+{
+	string result; // default construction is "", use result.empty() to verify
+	try
 	{
-		return true;
+		if (this->CheckValidProp(var_id))
+		{
+			for (const auto& [key, value] : entity_table_) // this is "Structured binding"
+			{
+				if ((value) == var_id) result = key;
+			}
+			return result;
+		}
+		else
+		{
+			throw invalid_argument("invalid ID");
+		}
 	}
+	catch (invalid_argument& e)
 	{
-		return false;
+		// TODO(Zhenlin): add error to the logger if possible
+		return result;
 	}
 }
--- Team39/Code39/src/spa/src/PKB/entity_table.h
+++ Team39/Code39/src/spa/src/PKB/entity_table.h
@@ -7,46 +7,99 @@
 
 using namespace std;
 
-typedef unsigned long long Ull;
-
 /*
 This is an abstract class for tables to record key infos for entities in SIMPLE program
 */
-
+template <typename T1, typename T2>
 class EntityTable
 {
 protected:
-	unordered_map<int, int> entity_table_;
-
-	// Check if the property index passed is a valid one
-	virtual bool CheckValidEntityIdx(int entity_idx);
+	// A hash table for entity with index with values being the property indicators of the entity
+	unordered_map<T1, T2> entity_table_;
 
 	// Return the size of the table
-	virtual Ull GetTableSize();
+	virtual int GetTableSize()
+	{
+		return static_cast<int>(entity_table_.size());
+	}
 
 private:
-	virtual bool CheckValidProp(int entity_prop) = 0;
+	// Check if the entity key passed in is a valid one
+	virtual bool CheckValidEntityKey(const T1 entity_key) = 0;
+
+	// Check if the entity property passed is a valid one
+	virtual bool CheckValidProp(const T2 entity_prop) = 0;
+
+	// Return if the table has the element with entity_key
+	virtual bool HasEntityWithKey(const T1 entity_key)
+	{
+		return entity_table_.find(entity_key) != entity_table_.end();
+	}
 
 public:
 	virtual ~EntityTable() = default;
 
-	// A hash table for entity with index with values being the property indicators of the entity
 	// WARNING: not safe if some lines are not inserted. E.g., 1,2,4 are present, but 3 is not. Calling 3 gives an internal error;
 
 	// Add a new entity into the table, returning 1 if successful, and 0 otherwise
-	virtual int AddEntity(int entity_idx, int entity_prop) = 0;
+	virtual int AddEntity(const T1 entity_key, const T2 entity_prop)
+	{
+		try
+		{
+			if (!CheckValidProp(entity_prop))
+			{
+				throw invalid_argument("Invalid property");
+			}
+			if (!CheckValidEntityKey(entity_key))
+			{
+				throw invalid_argument("Invalid key");
+			}
+			if (HasEntityWithKey(entity_key) && entity_table_[entity_key] != entity_prop)
+			{
+				//throw invalid_argument(to_string(entity_prop));
+				throw invalid_argument("Clashing with stored property [property_content here]");
+			}
+			entity_table_[entity_key] = entity_prop;
+			return 1;
+		}
+		catch (invalid_argument& e)
+		{
+			// TODO(Zhenlin): add error to the logger if possible
+			return 0;
+		}
+	}
 
 	// Get an entity via its index
-	virtual int GetEntityByIdx(int entity_idx) = 0;
-
-	// Get the set of entities with the same property via the property indicator value
-	virtual vector<int> GetEntityLstByProp(int entity_prop) = 0;
+	virtual int GetEntityByKey(T1 entity_key)
+	{
+		try
+		{
+			if (CheckValidEntityKey(entity_key))
+			{
+				return entity_table_[entity_key];
+			}
+			{
+				//throw invalid_argument(to_string(entity_idx));
+				throw invalid_argument("Invalid key");
+			}
+		}
+		catch (invalid_argument& e)
+		{
+			// TODO(Zhenlin): add error to the logger if possible
+			return 0;
+		}
+	}
 };
 
-class StmtTable final : public EntityTable
+
+/**
+ * The Statement table with keys being the statement index and values being the type of the statement
+ */
+class StmtTable final : public EntityTable<int,int>
 {
 private:
-	bool CheckValidProp(int stmt_prop) override;
+	bool CheckValidProp(const int stmt_prop) override;
+	bool CheckValidEntityKey(const int stmt_idx) override;
 
 public:
 	// TODO(Zhenlin): Check if replace following with Enum class after code review (can require quite a lot of refactoring though)
@@ -56,39 +109,38 @@ public:
 	static constexpr int call_idx_ = 6;
 	static constexpr int if_idx_ = 7;
 	static constexpr int while_idx_ = 8;
+
+	static constexpr int initial_index_ = 1;
 	
 
+	// TODO(Zhenlin): Check if virtual destructor needed here
 	StmtTable() = default;
 
-	// TODO(Zhenlin): Check if virtual destructor needed here
-
-	int AddEntity(int stmt_idx, int stmt_prop) override;
-
-	int GetEntityByIdx(int stmt_idx) override;
-
-	vector<int> GetEntityLstByProp(int stmt_prop) override;
+	// Get the set of entities with the same property via the property indicator value
+	vector<int> GetStmtLstByProp(int stmt_prop);
 
 };
 
-class NonStmtTable final : public EntityTable
+/**
+ * The table with keys being the non-statement entity name string and values being the index of the variable
+ */
+class NonStmtTable final : public EntityTable<string,int>
 {
 private:
-	bool CheckValidProp(int var_prop) override;
+	bool CheckValidProp(int var_id) override;
+	bool CheckValidEntityKey(const string var_name) override;
+	int entity_type_id_;
 
 public:
-	// TODO(Zhenlin): Check if replace following with Enum class after code review (can require quite a lot of refactoring though)
-	static constexpr int var_idx_ = 3;
-	static constexpr int const_idx_ = 4;
-	static constexpr int operator_idx_ = 5;
-	static constexpr int proc_idx_ = 6;
+	static constexpr int var_id_ = 1;
+	static constexpr int const_id_ = 2;
+	static constexpr int proc_id_ = 3;
+	static constexpr int operator_id_ = 4;
 
-	NonStmtTable() = default;
+	static constexpr int initial_id_ = 1;
 
 	// TODO(Zhenlin): Check if virtual destructor needed here
+	NonStmtTable(const int entity_type) : EntityTable{}, entity_type_id_{ entity_type } {}
 
-	int AddEntity(int var_idx, int var_prop) override;
-
-	int GetEntityByIdx(int var_idx) override;
-
-	vector<int> GetEntityLstByProp(int var_prop) override;
+	string GetVarById(int var_id);
 };
