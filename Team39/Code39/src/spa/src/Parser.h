056ae629815223b62548c89899b347760ecfae78
Lauren Lee <e0424895@u.nus.edu>
2022-02-01 22:48:23 +0800

    Add Parser files

--- Team39/Code39/src/spa/src/Parser.h
+++ Team39/Code39/src/spa/src/Parser.h
@@ -1,3 +1,26 @@
 #pragma once
+
+#include <optional>
+#include <string>
+
+#include "Tokenizer.h"
+
 using namespace std;
-int Parse();
+
+class Parser {
+public: 
+	void parse(vector<Token>& tokens);
+	void parseStmtLst(vector<Token>& tokens);
+
+private:
+	// Default identifier is empty string
+	optional<Token> expectIdentifier(const string& name = "");
+
+	optional<Token> expectBracket(const string& name = "");
+
+	optional<Token> expectSymbol(const string& name = "");
+
+	vector<Token>::iterator currToken;
+	vector<Token>::iterator endToken;
+};
+
