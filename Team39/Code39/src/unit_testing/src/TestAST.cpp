f24d96fac4c96e05582fb7a63a62d298ad220bf9
Lauren Lee <e0424895@u.nus.edu>
2022-02-01 22:53:41 +0800

    Modify TestAST.cpp to run tokenizer

--- Team39/Code39/src/unit_testing/src/TestAST.cpp
+++ Team39/Code39/src/unit_testing/src/TestAST.cpp
@@ -1,8 +1,14 @@
-
+#include "Tokenizer.h"
+#include "Parser.h"
 #include "TNode.h"
-
 #include "catch.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
 using namespace std;
+
 void require(bool b) {
     REQUIRE(b);
 }
@@ -15,5 +21,35 @@ TEST_CASE("1st Test") {
 	
     require(1 == 1);
 }
+
+TEST_CASE("Run tokenizer with file input") {
+	ifstream myFile("SourceProgramInput.txt"); // Input file is in same folder as unit_testing.exe
+	Tokenizer tokenizer;
+	Parser parser;
+	string sourceProg;
+	vector<Token> actualTokensList;
+
+	if (myFile.is_open()) {
+		while (getline(myFile, sourceProg))
+		{
+			vector<Token> actualTokensList = tokenizer.parse(sourceProg);
+
+			for (Token currToken : actualTokensList) {
+				currToken.print();
+			}
+			
+			if (actualTokensList.size() != 0) {
+				parser.parse(actualTokensList);
+			} else {
+				cout << "No tokens";
+			}
+		}
+		myFile.close();
+	} else {
+		cout << "Unable to open file";
+	}
+	require(1 == 1);
+}
+
 
 
