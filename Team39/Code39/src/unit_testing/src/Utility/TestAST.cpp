05443268ce68204784ffb49cad5d7a4e32a50d3b
Zhu Yuxuan <e0424709@u.nus.edu>
2022-02-02 11:02:54 +0800

    Initialize vectors properly

--- Team39/Code39/src/unit_testing/src/Utility/TestAST.cpp
+++ Team39/Code39/src/unit_testing/src/Utility/TestAST.cpp
@@ -30,8 +30,7 @@ TEST_CASE("Read - Test 1") {
 	list<string> input = { "read", "a", ";" };
 
 	TNode* stmt1 = new readTNode("1", "a");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
+	vector<TNode*> stmts = { stmt1 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -47,8 +46,7 @@ TEST_CASE("Read - Test 2") {
 	list<string> input = { "read", "a", ";" };
 
 	TNode* stmt1 = new printTNode("1", "a");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
+	vector<TNode*> stmts = { stmt1 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -65,9 +63,7 @@ TEST_CASE("Read - Test 3") {
 
 	TNode* stmt1 = new readTNode("1", "a");
 	TNode* stmt2 = new readTNode("2", "b");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
+	vector<TNode*> stmts = { stmt1, stmt2 };
 
 	AST expectedAST(new stmtLstTNode(stmts));
 
@@ -85,9 +81,7 @@ TEST_CASE("Read - Test 4") {
 
 	TNode* stmt1 = new readTNode("1", "a");
 	TNode* stmt2 = new printTNode("2", "b");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
+	vector<TNode*> stmts = { stmt1, stmt2 };
 
 	AST expectedAST(new stmtLstTNode(stmts));
 
@@ -107,12 +101,7 @@ TEST_CASE("Read - Test 5") {
 	TNode* stmt2 = new readTNode("2", "b");
 	TNode* stmt3 = new readTNode("3", "c");
 	TNode* stmt4 = new readTNode("4", "d");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
-	stmts.push_back(stmt3);
-	stmts.push_back(stmt4);
-
+	vector<TNode*> stmts = { stmt1, stmt2, stmt3, stmt4 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -128,8 +117,7 @@ TEST_CASE("Print - Test 1") {
 	list<string> input = { "print", "a", ";" };
 
 	TNode* stmt1 = new printTNode("1", "a");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
+	vector<TNode*> stmts = { stmt1 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -145,8 +133,7 @@ TEST_CASE("Print - Test 2") {
 	list<string> input = { "print", "a", ";" };
 
 	TNode* stmt1 = new readTNode("1", "a");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
+	vector<TNode*> stmts = { stmt1 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -163,9 +150,7 @@ TEST_CASE("Print - Test 3") {
 
 	TNode* stmt1 = new printTNode("1", "a");
 	TNode* stmt2 = new printTNode("2", "b");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
+	vector<TNode*> stmts = { stmt1, stmt2 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -182,9 +167,7 @@ TEST_CASE("Print - Test 4") {
 
 	TNode* stmt1 = new printTNode("1", "a");
 	TNode* stmt2 = new readTNode("2", "b");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
+	vector<TNode*> stmts = { stmt1, stmt2 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -203,11 +186,7 @@ TEST_CASE("Print - Test 5") {
 	TNode* stmt2 = new printTNode("2", "b");
 	TNode* stmt3 = new printTNode("3", "c");
 	TNode* stmt4 = new printTNode("4", "d");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
-	stmts.push_back(stmt3);
-	stmts.push_back(stmt4);
+	vector<TNode*> stmts = { stmt1, stmt2, stmt3, stmt4 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
@@ -226,11 +205,7 @@ TEST_CASE("Read and Print - Test 1") {
 	TNode* stmt2 = new readTNode("2", "b");
 	TNode* stmt3 = new printTNode("3", "c");
 	TNode* stmt4 = new printTNode("4", "d");
-	vector<TNode*> stmts;
-	stmts.push_back(stmt1);
-	stmts.push_back(stmt2);
-	stmts.push_back(stmt3);
-	stmts.push_back(stmt4);
+	vector<TNode*> stmts = { stmt1, stmt2, stmt3, stmt4 };
 	AST expectedAST(new stmtLstTNode(stmts));
 
 	AST actualAST;
