2f585f6458dbcd8dc871e580a2a583f01b77d4c9
Lauren Lee <e0424895@u.nus.edu>
2022-02-04 15:32:27 +0800

    Change Parser.cpp and .h files to default files

--- Team39/Code39/src/spa/src/Parser.cpp
+++ Team39/Code39/src/spa/src/Parser.cpp
@@ -1,114 +1,14 @@
-#include <stdio.h>
-
+#include<stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "Parser.h"
-#include "PKB.h"
-#include "TNode.h"
-
 using namespace std;
 
-// Checks for procedure, procedure name, and open curly bracket
-void Parser::parse(vector<Token>& tokens) {
-	currToken = tokens.begin(); 
-	endToken = tokens.end();
+#include "PKB/pkb.h"
+#include "Utility/TNode.h"
 
-	while (currToken != endToken) {	
-		optional<Token> possibleType = expectIdentifier();
-			
-		if (possibleType.has_value()) {
-			optional<Token> possibleName = expectIdentifier();
-				
-			if (possibleName.has_value()) { // Name found
-				optional<Token> possibleOpenBracket = expectBracket("{");
+int Parse() {
 
-				if (possibleOpenBracket.has_value()) { // Procedure found
-					cout << "Procedure: " << possibleName->text << endl;
-
-					while (currToken != endToken) {
-						parseStmtLst(tokens);
-					}
-				}
-			}
-		}
-	}
-	return;
-}
-
-// Checks for read and print statements, and semicolon
-void Parser::parseStmtLst(vector<Token>& tokens) {
-	optional<Token> possibleIdentifier = expectIdentifier();
-	optional<Token> possibleCloseBracket = expectBracket("}");
-
-	if (!possibleIdentifier.has_value()) {
-		return;
-	}
-
-	if (possibleCloseBracket.has_value()) {
-		cout << "Done";
-	}
-
-	if (possibleIdentifier->text == "read" || possibleIdentifier->text == "print") {
-		optional<Token> possibleVar = expectIdentifier();
-
-		if (possibleVar.has_value()) {
-			optional<Token> possibleSemicolon = expectSymbol(";");
-
-			if (possibleSemicolon.has_value()) {
-				cout << "Statement " << possibleSemicolon->stmtNum << ": " 
-					<< possibleIdentifier->text << " " << possibleVar->text << endl;
-			}
-		}
-	}
-}
-
-// Checks for possible identifiers (Names)
-optional<Token> Parser::expectIdentifier(const string& name) {
-	if (currToken->type != NAME
-		&& currToken->type != LETTER) {
-		return nullopt;
-	}
-
-	if (!name.empty() && currToken->text != name) {
-		return nullopt;
-	}
-
-	Token returnToken = *currToken;
-	currToken++; // Increment current token
-	return returnToken;
-}
-
-// Checks for possible brackets 
-optional<Token> Parser::expectBracket(const string& name) {
-	if (currToken->type != LEFT_CURLY
-		&& currToken->type != RIGHT_CURLY
-		&& currToken->type != LEFT_PAREN
-		&& currToken->type != RIGHT_PAREN) {
-		return nullopt;
-	}
-
-	if (!name.empty() && currToken->text != name) {
-		return nullopt;
-	}
-
-	Token returnToken = *currToken;
-	currToken++; // Increment current token
-	return returnToken;
-}
-
-// Checks for possible semicolon
-optional<Token> Parser::expectSymbol(const string& name) {
-	if (currToken->type != SEMICOLON) {
-		return nullopt;
-	}
-
-	if (!name.empty() && currToken->text != name) {
-		return nullopt;
-	}
-
-	Token returnToken = *currToken;
-	currToken++; // Increment current token
-	return returnToken;
+  return 0;
 }
--- Team39/Code39/src/spa/src/Parser.h
+++ Team39/Code39/src/spa/src/Parser.h
@@ -1,26 +1,3 @@
 #pragma once
-
-#include <optional>
-#include <string>
-
-#include "Tokenizer.h"
-
 using namespace std;
-
-class Parser {
-public: 
-	void parse(vector<Token>& tokens);
-	void parseStmtLst(vector<Token>& tokens);
-
-private:
-	// Default identifier is empty string
-	optional<Token> expectIdentifier(const string& name = "");
-
-	optional<Token> expectBracket(const string& name = "");
-
-	optional<Token> expectSymbol(const string& name = "");
-
-	vector<Token>::iterator currToken;
-	vector<Token>::iterator endToken;
-};
-
+int Parse();
