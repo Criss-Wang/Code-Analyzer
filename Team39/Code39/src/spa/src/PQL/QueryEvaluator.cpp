ae4a3ce54526198d38db8d1589152367a9711a2b
Gj <guojunloh1234@gmail.com>
2022-02-05 23:11:03 +0800

    Update QueryEvaluator.cpp to deal with pattern clause

--- Team39/Code39/src/spa/src/PQL/QueryEvaluator.cpp
+++ Team39/Code39/src/spa/src/PQL/QueryEvaluator.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_multiset>
 
 using namespace std;
 
@@ -9,12 +11,41 @@ using namespace std;
 #include "PKB.h"
 
 vector<string> evaluate(Query query) {
-	Query::EntityType entType = query.getSelectType();
-	Clause[] clauses = query.getClauses();
+	Clause suchThatClause = query.getSuchThatClause();
+	Clause patternClause = query.getPatternClause();
+	unordered_map<Synonym, list<int>> hashmap;
+	vector<Synonym> synonyms = query.getAllSynonymsUsed();
 
-	if (clauses.length == 0) {
-		PKB::getAllEntity(entType);
+	//hashmap stores <synonym, domain> pair.
+	for (const Synonym& synonym : synonyms) {
+		hahsmap.insert({ synonym, PKB::getAllEntity(synonym.entType)});
 	}
 
+	if (patternClause != NULL) {
+        Synonym s = patternClause.getSynonym();
+
+        
+	}
+
+}
+
+void updateHashmap(const unordered_map& hashmap, const Synonym & syn, const list<int> & lst) {
+    //took from stack overflow https://stackoverflow.com/questions/38993415/how-to-apply-the-intersection-between-two-lists-in-c
+    list<int> res;
+    lst<int> oriDomain = hashmap[*syn];
+    unordered_multiset<int> st;
+    for_each(lst.begin(), lst.end(), [&st](const T& k) { st.insert(k); });
+    for_each(oriDomain.begin(), oriDomain.end(),
+        [&st, &res](const T& k) {
+            auto iter = st.find(k);
+            if (iter != st.end()) {
+                rtn.push_back(k);
+                st.erase(iter);
+            }
+        }
+    );
+    
+    hashmap.emplace(*syn, res);
+    
 }
 
