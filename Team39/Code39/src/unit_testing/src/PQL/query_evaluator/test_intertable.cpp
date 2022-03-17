cc018d55d19784550c06849cd745f2e72a7f35f9
Gj <guojunloh1234@gmail.com>
2022-03-17 16:14:16 +0800

    Add test case for GetColsByIndices function

--- Team39/Code39/src/unit_testing/src/PQL/query_evaluator/test_intertable.cpp
+++ Team39/Code39/src/unit_testing/src/PQL/query_evaluator/test_intertable.cpp
@@ -14,7 +14,7 @@ using namespace std;
 //3 columns in a table
 
 // table1 represents table with one col, table2 represents table with 2 col, ...
-/*-----------------------------------------TABLES with one column ---------------------------------------------------- */
+/*-------------------------------------------------------TABLES with one column ------------------------------------------------------------------ */
 //Table with one column and elements are integer
 static vector<int> table1_col({ 1,2,3,4,5 });
 static pql::Synonym table1_syn("s1", EntityIdentifier::kStmt);
@@ -25,9 +25,9 @@ static pql_table::InterTable table1_out_order = pql_table::InterTable(table1_syn
 
 static vector<int> table1_duplicate_col({ 1,1,1,2,3,3,4,4,4,4,5 });
 static pql_table::InterTable table1_duplicate = pql_table::InterTable(table1_syn, table1_duplicate_col);
-/*--------------------------------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-/*----------------------------------------TABLES with two columns --------------------------------------------------- */ 
+/*------------------------------------------------------TABLES with two columns ------------------------------------------------------------------ */ 
 // standard table with 2 columns
 static vector<int> table2_first_col({ 2, 4, 6, 8, 10 });
 static vector<int> table2_second_col({ 1, 3, 5, 7, 9 });
@@ -67,6 +67,10 @@ static pql::Synonym table4_first_syn("s", EntityIdentifier::kStmt);
 static pql::Synonym table4_second_syn("s1", EntityIdentifier::kStmt);
 static pql::Synonym table4_third_syn("c", EntityIdentifier::kCall);
 static pql::Synonym table4_fourth_syn("p", EntityIdentifier::kProc);
+static string table4_first_syn_name = table4_first_syn.GetName();
+static string table4_second_syn_name = table4_second_syn.GetName();
+static string table4_third_syn_name = table4_third_syn.GetName();
+static string table4_fourth_syn_name = table4_fourth_syn.GetName();
 
 static vector<string> table2_merge_into_table4_1_header({ table4_first_syn.GetName(), table4_second_syn.GetName() });
 static vector<vector<int>> table2_merge_into_table4_1_rows = { {3, 13}, {19, 14}, {2, 16}, {10, 18}, {9, 5} };
@@ -76,16 +80,32 @@ static vector<string> table2_merge_into_table4_2_header({ table4_third_syn.GetNa
 static vector<vector<int>> table2_merge_into_table4_2_rows = { {14, 5}, {2, 14}, {10, 9} };
 static pql_table::InterTable table2_merge_into_table4_2(table2_merge_into_table4_2_header, table2_merge_into_table4_2_rows);
 
-/*-----------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-/*------------------------------------------TABLES with four columns ------------------------------------------------------- */
+/*--------------------------------------------------------TABLES with four columns ---------------------------------------------------------------------- */
 static vector<string> table4_header({ table4_first_syn.GetName(), table4_second_syn.GetName(), table4_third_syn.GetName(), table4_fourth_syn.GetName() });
 static vector<vector<int>> table4_rows = { {3, 13, 14, 5}, {3, 13, 2, 14}, {3, 13, 10, 9}, {19, 14, 14, 5}, {19, 14, 2, 14}, {19, 14, 10, 9},
 		{2, 16, 14, 5}, {2, 16, 2, 14}, {2, 16, 10, 9}, {10, 18, 14, 5}, {10, 18, 2, 14}, {10, 18, 10, 9}, {9, 5, 14, 5}, {9, 5, 2, 14}, {9, 5, 10, 9} };
 static pql_table::InterTable table4(table4_header, table4_rows);
 
+static vector<vector<int>> table4_duplicate_rows = { {3, 13, 14, 5}, {3, 13, 2, 14}, {3, 13, 10, 9}, {19, 14, 14, 5}, {19, 14, 2, 14}, {19, 14, 10, 9},
+		{2, 16, 14, 5}, {2, 16, 2, 14}, {2, 16, 10, 9}, {10, 18, 14, 5}, {10, 18, 2, 14}, {10, 18, 10, 9}, {9, 5, 14, 5}, {9, 5, 2, 14}, {9, 5, 10, 9},
+    {3, 13, 14, 5}, {3, 13, 2, 14}, {3, 13, 10, 9}, {19, 14, 14, 5}, {19, 14, 2, 14}, {2, 16, 14, 5}, {2, 16, 2, 14}, {2, 16, 14, 5}, {2, 16, 2, 14} };
+static pql_table::InterTable table4_duplicate(table4_header, table4_duplicate_rows);
 
-/*----------------------------------------------------------------------------------------------------------------------------*/
+static vector<pair<int, int>> table4_pred_lst_1 = { make_pair(1, 2), make_pair(7, 5), make_pair(13, 14),
+		make_pair(13, 10), make_pair(14, 10), make_pair(16, 14), make_pair(16, 2), make_pair(18, 10) };
+static pql_table::Predicate table4_pred_1(table4_second_syn_name, table4_third_syn_name, table4_pred_lst_1);
+
+static vector<vector<int>> table4_filter_1_rows = { {3, 13, 14, 5}, {3, 13, 10, 9}, {19, 14, 10, 9}, {2, 16, 14, 5}, {2, 16, 2, 14}, {10, 18, 10, 9}};
+static pql_table::InterTable table4_filter_1(table4_header, table4_filter_1_rows);
+
+static vector<pair<int, int>> table4_pred_lst_2 = { make_pair(13, 3), make_pair(18, 10) };
+static pql_table::Predicate table4_pred_2(table4_second_syn_name, table4_first_syn_name, table4_pred_lst_2);
+
+static vector<vector<int>> table4_filter_2_rows = { {3, 13, 14, 5}, {3, 13, 2, 14}, {3, 13, 10, 9}, {10, 18, 14, 5}, {10, 18, 2, 14}, {10, 18, 10, 9} };
+static pql_table::InterTable table4_filter_2(table4_header, table4_filter_2_rows);
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 TEST_CASE("Check equality of InterTable") {
 	SECTION("Tables should be equal if the contents are the same, order does not matter") {
@@ -111,6 +131,9 @@ TEST_CASE("Check Deduplicate function of Intertable") {
 
 		REQUIRE(!table2.equal(table2_duplicate));
 		REQUIRE(table2.equal(table2_duplicate.Deduplicate()));
+
+		REQUIRE(!table4_duplicate.equal(table4));
+		REQUIRE(table4_duplicate.Deduplicate().equal(table4));
 	}
 }
 
@@ -131,24 +154,74 @@ TEST_CASE("Check merge function of Intertable") {
 
 TEST_CASE("Check filter function of Intertable") {
   
-  SECTION("Filter tables with content") {
+  SECTION("Filter tables of two columns with content") {
 		REQUIRE(!table2_merge.equal(table2_filter));
 		REQUIRE(table2_merge.Filter(nonempty_predicates).equal(table2_filter));
   }
 
-	SECTION("The table after filter will be empty") {
+	SECTION("The table of two columns after filter will be empty") {
 	  CHECK_THROWS_AS(table2_merge.Filter(empty_predicates), pql_exceptions::EmptyTableException);
+	}
+
+	SECTION("Filter tables of four columns with content") {
+	  REQUIRE(!table4.equal(table4_filter_1));
+		REQUIRE(table4.Filter(table4_pred_1).equal(table4_filter_1));
+
+		REQUIRE(!table4.equal(table4_filter_2));
+		REQUIRE(table4.Filter(table4_pred_2).equal(table4_filter_2));
 	}
 }
 
 TEST_CASE("Check MergeAndFilter function of Intertable") {
 
-  SECTION("MergeAndFilter tables with content") {
+  SECTION("MergeAndFilter tables of one column with content") {
 		REQUIRE(!table1_merge1.equal(table2_filter));
 		REQUIRE(table1_merge1.MergeAndFilter(table1_merge2, nonempty_predicates).equal(table2_filter));
   }
 
-	SECTION("The table after merge and filter will be empty") {
+	SECTION("The table with two columns after merge and filter will be empty") {
 		CHECK_THROWS_AS(table1_merge1.MergeAndFilter(table1_merge2, empty_predicates).equal(table2_filter), pql_exceptions::EmptyTableException);
+	}
+
+	SECTION("MergeAndFilter tables of two columns with content") {
+	  REQUIRE(table2_merge_into_table4_1.MergeAndFilter(table2_merge_into_table4_2, table4_pred_1).equal(table4_filter_1));
+	}
+}
+
+TEST_CASE("Check GetColByIndices function of InterTable") {
+	SECTION("GetColByIndices on tables with four column") {
+	  
+	  SECTION("Get one column") {
+			vector<string> res1_header = { table4_first_syn_name };
+			vector<vector<int>> res1_rows = { { 3 }, { 19 }, { 2 }, { 10 }, { 9 } };
+			pql_table::InterTable res1_table(res1_header, res1_rows);
+			
+			vector<vector<int>> res1_duplicate_rows = { { 3 }, { 3 }, { 3 }, { 19 }, { 19 }, { 19 }, { 2 }, { 2 }, { 2 }, 
+					{ 10 }, { 10 }, { 10 }, { 10 }, { 9 }, { 9 }, { 9 } };
+			pql_table::InterTable res1_duplicate_table(res1_header, res1_duplicate_rows);
+
+			vector<int> index_lst = { 0 };
+			REQUIRE(table4.GetColsByIndices(index_lst).equal(res1_table));
+			REQUIRE(!table4.GetColsByIndices(index_lst).equal(res1_duplicate_table)); //the GetColsByIndices return an InterTable without any duplication
+		}
+		
+		SECTION("Get two columns") {
+		  vector<string> res2_header = { table4_second_syn_name, table4_fourth_syn_name };
+		  vector<vector<int>> res2_rows = { { 13, 5 }, { 13, 14 }, { 13, 9 }, { 14, 5 }, { 14, 14 }, { 14, 9 }, 
+					{ 16, 5 }, { 16, 14 }, { 16, 9 }, { 18, 5 }, { 18, 14 }, { 18, 9 }, { 5, 5 }, { 5, 14 }, { 5, 9 } };
+		  pql_table::InterTable res2_table(res2_header, res2_rows);
+		  vector<int> index_lst = { 1, 3 };
+		  REQUIRE(table4.GetColsByIndices(index_lst).equal(res2_table));
+		}
+
+		SECTION("Get three columns") {
+		  vector<string> res3_header = { table4_second_syn_name, table4_third_syn_name, table4_fourth_syn_name };
+		  vector<vector<int>> res3_rows = { { 13, 14, 5 }, { 13, 2, 14 }, { 13, 10, 9 }, { 14, 14, 5 }, { 14, 2, 14 }, { 14, 10, 9 },
+				{ 16, 14, 5 }, { 16, 2, 14 }, { 16, 10, 9 }, { 18, 14, 5 }, { 18, 2, 14 }, { 18, 10, 9 }, { 5, 14, 5 }, { 5, 2, 14 }, { 5, 10, 9 } };
+		  pql_table::InterTable res3_table(res3_header, res3_rows);
+		  vector<int> index_lst = { 1, 2, 3 };
+		  REQUIRE(table4.GetColsByIndices(index_lst).equal(res3_table));
+		}
+
 	}
 }
