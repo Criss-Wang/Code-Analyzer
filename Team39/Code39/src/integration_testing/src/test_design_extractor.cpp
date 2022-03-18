ed41fcb14dcdb5484165d59c5fb5d35c1de9aa79
Zheng <limzhengwei_99@hotmail.com>
2022-03-18 16:47:43 +0800

    Add integration tests for UsesS with calls

--- Team39/Code39/src/integration_testing/src/test_design_extractor.cpp
+++ Team39/Code39/src/integration_testing/src/test_design_extractor.cpp
@@ -563,7 +563,7 @@ TEST_CASE("Test Nested Population for ModifiesP") {
    }
 }
 
-TEST_CASE("Test Nested Population for ModifiesS Calls") {
+TEST_CASE("Test Nested Population for ModifiesS with Calls") {
   Pkb pkb = Pkb();
   /*
    *    p1 {
@@ -905,5 +905,302 @@ TEST_CASE("Test Nested Population for ModifiesS Calls") {
     std::sort(stmts.begin(), stmts.end());
     std::sort(expected_stmts.begin(), expected_stmts.end());
     REQUIRE(expected_stmts == stmts);
+  }
+}
+
+TEST_CASE("Test Nested Population for UsesS with Calls") {
+  Pkb pkb = Pkb();
+  /*
+   *    p1 {
+   * 1.   a = a * 2;
+   * 2.   a = b * 2;
+   * 3.   call p4;
+   * 4.   while (first < 2) {
+   * 5.     a = c * 2;
+   * 6.     call p6;
+   * 7.     while (second == 10) {
+   * 8.       a = d * 2;
+   * 9.       call p5;
+   *        }
+   *      }
+   *
+   *    p5 {
+   * 10.  a = e * 2;
+   * 11.  a = f * 2;
+   *    }
+   *
+   *    p4 {
+   * 12.  a = x * 2;
+   * 13.  a = y * 2;
+   * 14.  a = z * 2;
+   *    }
+   *
+   *    p6 {
+   * 15.  a = w * 2;
+   * 16.  call p8;
+   *    }
+   *
+   *    p8 {
+   * 17.  a = v * 2;
+   * 18.  if (third == 3) then {
+   * 19.    a = awesome * hmm * great;
+   *      } else {
+   * 20.    a = hello + there;
+   *      }
+   *    }
+   */
+
+  /* Calls
+   * p1 -> p4, p6, p5
+   * p6 -> p8
+   *
+   * Calls*
+   * p1 -> p4, p6, p5, p8
+   * p6 -> p8
+   *
+   * Parent
+   * 4 -> 5, 6, 7
+   * 7 -> 8, 9
+   * 18 -> 19, 20
+   *
+   * Parent*
+   * 4 -> 5, 6, 7, 8, 9
+   * 7 -> 8, 9
+   * 18 -> 19, 20
+   *
+   * UsesProcToVariables
+   * p1 -> a, b, first, c, second, d
+   * p5 -> e, f
+   * p4 -> x, y, z
+   * p6 -> w
+   * p8 -> v, third, awesome, hmm, great, hello, there
+   *
+   * UsesProcToVariables with nested
+   * p1 -> a, b, first, c, second, d, x, y, z, w, v, third, awesome,
+   *       hmm, great, hello, there, e, f
+   * p5 -> e, f
+   * p4 -> x, y, z
+   * p6 -> w, v, third, awesome, hmm, great, hello, there
+   * p8 -> v, third, awesome, hmm, great, hello, there
+   *
+   * UsesStmtToVars
+   * 1 -> a
+   * 2 -> b
+   * 4 -> first
+   * 5 -> c
+   * 7 -> second
+   * 8 -> d
+   * 10 -> e
+   * 11 -> f
+   * 12 -> x
+   * 13 -> y
+   * 14 -> z
+   * 15 -> w
+   * 17 -> v
+   * 18 -> third
+   * 19 -> awesome, hmm, great
+   * 20 -> hello, there
+   *
+   * UsesStmtToVars including nested but without calls
+   * 1 -> a
+   * 2 -> b
+   * 4 -> first, c, second, d
+   * 5 -> c
+   * 7 -> second, d
+   * 8 -> d
+   * 10 -> e
+   * 11 -> f
+   * 12 -> x
+   * 13 -> y
+   * 14 -> z
+   * 15 -> w
+   * 17 -> v
+   * 18 -> third, awesome, hmm, great, hello, there
+   * 19 -> awesome, hmm, great
+   * 20 -> hello, there
+   *
+   * UsesStmtToVars including nested and calls
+   * 1 -> a
+   * 2 -> b
+   * 3 -> x, y, z
+   * 4 -> first, c, second, d, e, f,
+   *      w, v, third, awesome, hmm, great, hello, there
+   * 5 -> c
+   * 6 -> w, v, third, awesome, hmm, great, hello, there
+   * 7 -> second, d, e, f
+   * 8 -> d
+   * 9 -> e, f
+   * 10 -> e
+   * 11 -> f
+   * 12 -> x
+   * 13 -> y
+   * 14 -> z
+   * 15 -> w
+   * 16 -> v, third, awesome, hmm, great, hello, there
+   * 17 -> v
+   * 18 -> third, awesome, hmm, great, hello, there
+   * 19 -> awesome, hmm, great
+   * 20 -> hello, there
+   */
+
+  bool success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "a");
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "b") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "c") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "d") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "e") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "f") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "v") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "w") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "x") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "y") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "z") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "first") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "second") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "third") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "awesome") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "hmm") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "great") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "hello") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kVariable, "there") && success;
+
+  success = pkb.AddEntityToSet(EntityIdentifier::kProc, "p1") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kProc, "p4") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kProc, "p5") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kProc, "p6") && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kProc, "p8") && success;
+
+  success = pkb.AddEntityToSet(EntityIdentifier::kCall, 3) && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kCall, 6) && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kCall, 9) && success;
+  success = pkb.AddEntityToSet(EntityIdentifier::kCall, 16) && success;
+
+  success = pkb.AddInfoToTable(TableIdentifier::kParent, 4, vector<int>{5, 6, 7}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kParent, 7, vector<int>{8, 9}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kParent, 18, vector<int>{19, 20}) && success;
+
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 1, vector<string>{"a"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 2, vector<string>{"b"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 4, vector<string>{"first"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 5, vector<string>{"c"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 7, vector<string>{"second"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 8, vector<string>{"d"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 10, vector<string>{"e"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 11, vector<string>{"f"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 12, vector<string>{"x"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 13, vector<string>{"y"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 14, vector<string>{"z"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 15, vector<string>{"w"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 17, vector<string>{"v"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 18, vector<string>{"third"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 19, vector<string>{"awesome", "hmm", "great"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesStmtToVar, 20, vector<string>{"hello", "there"}) && success;
+
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesProcToVar, "p1", vector<string>{"a", "b", "first", "c", "second", "d"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesProcToVar, "p5", vector<string>{"e", "f"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesProcToVar, "p4", vector<string>{"x", "y", "z"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesProcToVar, "p6", vector<string>{"w"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kUsesProcToVar, "p8", vector<string>{"v", "third", "awesome", "hmm", "great", "hello", "there"}) && success;
+
+  success = pkb.AddInfoToTable(TableIdentifier::kCalls, "p1", vector<string>{"p4", "p5", "p6"}) && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kCalls, "p6", vector<string>{"p8"}) && success;
+
+  success = pkb.AddInfoToTable(TableIdentifier::kCaller, 3, "p4") && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kCaller, 6, "p6") && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kCaller, 9, "p5") && success;
+  success = pkb.AddInfoToTable(TableIdentifier::kCaller, 16, "p8") && success;
+
+  // Populate nested
+  success = success && PopulateNestedRelationships(pkb);
+
+  int a_idx = pkb.GetIndexByVar("a");
+  int b_idx = pkb.GetIndexByVar("b");
+  int c_idx = pkb.GetIndexByVar("c");
+  int d_idx = pkb.GetIndexByVar("d");
+  int e_idx = pkb.GetIndexByVar("e");
+  int f_idx = pkb.GetIndexByVar("f");
+  int v_idx = pkb.GetIndexByVar("v");
+  int w_idx = pkb.GetIndexByVar("w");
+  int x_idx = pkb.GetIndexByVar("x");
+  int y_idx = pkb.GetIndexByVar("y");
+  int z_idx = pkb.GetIndexByVar("z");
+  int first_idx = pkb.GetIndexByVar("first");
+  int second_idx = pkb.GetIndexByVar("second");
+  int third_idx = pkb.GetIndexByVar("third");
+  int awesome_idx = pkb.GetIndexByVar("awesome");
+  int hmm_idx = pkb.GetIndexByVar("hmm");
+  int great_idx = pkb.GetIndexByVar("great");
+  int hello_idx = pkb.GetIndexByVar("hello");
+  int there_idx = pkb.GetIndexByVar("there");
+
+  int p1_idx = pkb.GetIndexByProc("p1");
+  int p4_idx = pkb.GetIndexByProc("p4");
+  int p5_idx = pkb.GetIndexByProc("p5");
+  int p6_idx = pkb.GetIndexByProc("p6");
+  int p8_idx = pkb.GetIndexByProc("p8");
+
+  SECTION("Check population success") {
+    REQUIRE(success);
+  }
+
+  SECTION("Check calls* population before testing UsesS calls") {
+    vector<pair<int, int>> expected_transitive_calls_pairs = {make_pair(p1_idx, p4_idx), make_pair(p1_idx, p5_idx), make_pair(p1_idx, p6_idx),
+      make_pair(p1_idx, p8_idx), make_pair(p6_idx, p8_idx)};
+    vector<pair<int, int>> transitive_calls_pairs = pkb.GetAllTransitiveCallsPairs();
+    std::sort(expected_transitive_calls_pairs.begin(), expected_transitive_calls_pairs.end());
+    std::sort(transitive_calls_pairs.begin(), transitive_calls_pairs.end());
+    REQUIRE(transitive_calls_pairs == expected_transitive_calls_pairs);
+  }
+
+  SECTION("Check Parent* population before testing UsesS calls") {
+    vector<pair<int, int>> expected_transitive_parent_pairs = {make_pair(4, 5), make_pair(4, 6), make_pair(4, 7),
+      make_pair(4, 8), make_pair(4, 9), make_pair(7, 8), make_pair(7, 9), make_pair(18, 19), make_pair(18, 20)};
+    vector<pair<int, int>> transitive_parent_pairs = pkb.GetAllTransitiveParentPairs();
+    std::sort(expected_transitive_parent_pairs.begin(), expected_transitive_parent_pairs.end());
+    std::sort(transitive_parent_pairs.begin(), transitive_parent_pairs.end());
+    REQUIRE(transitive_parent_pairs == expected_transitive_parent_pairs);
+  }
+
+  SECTION("Check UsesP population before testing UsesS calls") {
+    vector<pair<int, int>> expected_proc_var_pairs = {make_pair(p1_idx, a_idx), make_pair(p1_idx, b_idx), make_pair(p1_idx, first_idx),
+      make_pair(p1_idx, c_idx), make_pair(p1_idx, second_idx), make_pair(p1_idx, d_idx), make_pair(p1_idx, e_idx), make_pair(p1_idx, f_idx),
+      make_pair(p1_idx, v_idx), make_pair(p1_idx, w_idx),
+      make_pair(p1_idx, x_idx), make_pair(p1_idx, y_idx), make_pair(p1_idx, z_idx), make_pair(p1_idx, third_idx), make_pair(p1_idx, awesome_idx),
+      make_pair(p1_idx, hmm_idx), make_pair(p1_idx, great_idx), make_pair(p1_idx, hello_idx), make_pair(p1_idx, there_idx),
+      make_pair(p5_idx, e_idx), make_pair(p5_idx, f_idx), make_pair(p4_idx, x_idx), make_pair(p4_idx, y_idx),
+      make_pair(p4_idx, z_idx), make_pair(p6_idx, v_idx), make_pair(p6_idx, w_idx),
+      make_pair(p6_idx, third_idx), make_pair(p6_idx, awesome_idx), make_pair(p6_idx, hmm_idx), make_pair(p6_idx, great_idx),
+      make_pair(p6_idx, hello_idx), make_pair(p6_idx, there_idx),
+      make_pair(p8_idx, v_idx), make_pair(p8_idx, third_idx), make_pair(p8_idx, awesome_idx), make_pair(p8_idx, hmm_idx), make_pair(p8_idx, great_idx),
+      make_pair(p8_idx, hello_idx), make_pair(p8_idx, there_idx)
+    };
+    vector<pair<int, int>> proc_var_pairs = pkb.GetAllUsesProcVarPairs();
+    std::sort(expected_proc_var_pairs.begin(), expected_proc_var_pairs.end());
+    std::sort(proc_var_pairs.begin(), proc_var_pairs.end());
+    REQUIRE(proc_var_pairs == expected_proc_var_pairs);
+  }
+
+  SECTION("Check UsesS with calls population") {
+    vector<pair<int, int>> expected_stmt_var_pairs = {make_pair(1, a_idx), make_pair(2, b_idx), make_pair(3, x_idx), make_pair(3, y_idx), make_pair(3, z_idx),
+      make_pair(4, first_idx), make_pair(4, c_idx), make_pair(4, second_idx), make_pair(4, d_idx), make_pair(4, e_idx), make_pair(4, f_idx),
+      make_pair(4, w_idx), make_pair(4, v_idx), make_pair(4, third_idx), make_pair(4, awesome_idx), make_pair(4, hmm_idx), make_pair(4, great_idx),
+      make_pair(4, hello_idx), make_pair(4, there_idx),
+      make_pair(5, c_idx),
+      make_pair(6, w_idx), make_pair(6, v_idx), make_pair(6, third_idx), make_pair(6, awesome_idx), make_pair(6, hmm_idx), make_pair(6, great_idx),
+      make_pair(6, hello_idx), make_pair(6, there_idx),
+      make_pair(7, second_idx), make_pair(7, d_idx), make_pair(7, e_idx), make_pair(7, f_idx),
+      make_pair(8, d_idx),
+      make_pair(9, e_idx), make_pair(9, f_idx), make_pair(10, e_idx), make_pair(11, f_idx), make_pair(12, x_idx), make_pair(13, y_idx), make_pair(14, z_idx),
+      make_pair(15, w_idx),
+      make_pair(16, v_idx), make_pair(16, third_idx), make_pair(16, awesome_idx), make_pair(16, hmm_idx), make_pair(16, great_idx),
+      make_pair(16, hello_idx), make_pair(16, there_idx),
+      make_pair(17, v_idx), make_pair(18, third_idx), make_pair(18, awesome_idx), make_pair(18, hmm_idx), make_pair(18, great_idx),
+      make_pair(18, hello_idx), make_pair(18, there_idx),
+      make_pair(19, awesome_idx), make_pair(19, hmm_idx), make_pair(19, great_idx),
+      make_pair(20, hello_idx), make_pair(20, there_idx)
+    };
+    vector<pair<int, int>> stmt_var_pairs = pkb.GetAllUsesStmtVarPairs();
+    std::sort(expected_stmt_var_pairs.begin(), expected_stmt_var_pairs.end());
+    std::sort(stmt_var_pairs.begin(), stmt_var_pairs.end());
+    REQUIRE(stmt_var_pairs == expected_stmt_var_pairs);
   }
 }
